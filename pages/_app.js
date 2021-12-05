b7710c9bb076408596388190db9726d40bc90308
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-12-05 15:24:55 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import "../styles/globals.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../firebase";
+import Login from "./Login";
+import Loading from "../components/Loading";
+import { collection, doc, setDoc, Timestamp } from "firebase/firestore";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      const userDetails = collection(db, "users");
+      setDoc(
+        doc(userDetails, user.uid),
+        {
+          email: user.email,
+          lastSeen: Timestamp.now(),
+          photoURL: user.photoURL,
+        },
+        { merge: true }
+      );
+    }
+  }, [user]);
+
+  if (loading) return <Loading />;
+  if (!user) return <Login />;
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
