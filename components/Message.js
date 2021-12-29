cc2f6264270e6fc8a0d7817270527cf37d9a5656
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-12-29 21:02:51 +0300

    Update Message.js

--- components/Message.js
+++ components/Message.js
@@ -2,16 +2,38 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
 import { auth } from "../firebase";
 import moment from "moment";
-
+import { Button, Avatar } from "@material-ui/core";
 function Message({ user, message }) {
   const [userLoggedIn] = useAuthState(auth);
 
-  const TypeOfMessage = user === userLoggedIn.email ? Sender : Reciever;
-
+  const TypeOfMessage = user.user === userLoggedIn.email ? Sender : Reciever;
   return (
     <Container>
       <TypeOfMessage>
-        {message.message}
+        {TypeOfMessage === Reciever ? (
+          <div>
+            <div style={{ display: "inline-block" }}>
+              <UserAvatar
+                style={{
+                  display: "inline-block",
+                  margin: 5,
+                  backgroundColor: "transparent",
+                }}
+                src={user.photoURL}
+              />
+            </div>
+            <div style={{ display: "inline-block" }}>{message.message}</div>
+          </div>
+        ) : (
+          <div>
+            {message.message}
+            <UserAvatar
+              style={{ display: "inline-block" }}
+              src={user.photoURL}
+            />
+          </div>
+        )}
+
         <Timestamp>
           {message.timestamp ? moment(message.timestamp).format("LT") : "..."}
         </Timestamp>
@@ -53,4 +75,12 @@ const Timestamp = styled.span`
   bottom: 0;
   text-align: right;
   right: 0;
+`;
+
+const UserAvatar = styled(Avatar)`
+  display: inline-block
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
 `;
